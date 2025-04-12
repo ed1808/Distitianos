1ec62eccb33f4df0ec909f2a37eabe9dc080ebcb
Edward Alexander Rodríguez Londoño <122028804+ed1808@users.noreply.github.com>
2025-04-11 21:28:44 -0500

    Modified test function

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,17 @@
 import { Database } from './src/core/database/Database';
 import { type Department } from './src/departments/models/Department';
+import type { CategoryCreate, Category } from './src/categories/models/Category';
 
 const dbInstance = Database.instance;
 
 const test = async () => {
-    console.log(await dbInstance.select<Department[]>(
+    /* console.log(await dbInstance.select<Department[]>(
         'departments', 
         ['id', 'department_name', 'department_code', 'active']
-    ));
+    )); */
+    /* const testCategory: CategoryCreate = {category_name: "Test"};
+    console.log(await dbInstance.insert<Category[]>('categories', testCategory)); */
+    // await dbInstance.delete('categories', {"id": 1});
 }
 
 test();

944ee711b9ac63dacf94384b7db4f0a5646c9bb5
Edward Alexander Rodríguez Londoño <122028804+ed1808@users.noreply.github.com>
2025-04-13 20:58:23 -0500

    Updated entry point

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,84 @@
+import { serve } from 'bun';
+
+import { Router } from './src/core/router/Router';
 import { Database } from './src/core/database/Database';
-import { type Department } from './src/departments/models/Department';
-import type { CategoryCreate, Category } from './src/categories/models/Category';
+import type { City } from './src/locations/models/City';
+import { HttpStatusCode } from './src/core/router/utils/HttpStatusCodes';
 
-const dbInstance = Database.instance;
+const router = new Router();
+const database = Database.instance;
 
-const test = async () => {
-    /* console.log(await dbInstance.select<Department[]>(
-        'departments', 
-        ['id', 'department_name', 'department_code', 'active']
-    )); */
-    /* const testCategory: CategoryCreate = {category_name: "Test"};
-    console.log(await dbInstance.insert<Category[]>('categories', testCategory)); */
-    // await dbInstance.delete('categories', {"id": 1});
-}
+router.use(req => {
+  console.log(
+    `[${new Date().toISOString()}] ${req.req.method} ${new URL(req.url).pathname}`
+  );
+  return null;
+});
 
-test();
+router.get('/api/locations/cities', async ({ query }) => {
+  const { limit, offset } = query;
+  const result = await database.select<City[]>(
+    'cities',
+    ['id', 'city_name', 'city_code'],
+    undefined,
+    undefined,
+    offset ? Number(offset) : undefined,
+    limit ? Number(limit) : undefined
+  );
+
+  if (result instanceof Error) {
+    return Response.json(
+      { message: result.message },
+      {
+        status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+
+  const cities = result.slice(0, result.length);
+
+  return Response.json(
+    { message: cities },
+    {
+      headers: { 'Content-Type': 'application/json' },
+    }
+  );
+});
+
+router.get('/api/locations/cities/:id', async ({ params }) => {
+  const { id } = params;
+  const result = await database.select<City[]>(
+    'cities',
+    ['id', 'city_name', 'city_code'],
+    { id: id! }
+  );
+
+  if (result instanceof Error) {
+    return Response.json(
+      { message: result.message },
+      {
+        status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+
+  const city = result.slice(0, result.length);
+
+  return Response.json(
+    { message: city },
+    {
+      headers: { 'Content-Type': 'application/json' },
+    }
+  );
+});
+
+const server = serve({
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  async fetch(req) {
+    return router.handle(req);
+  },
+});
+
+console.log(`Server running on port ${server.port}`);

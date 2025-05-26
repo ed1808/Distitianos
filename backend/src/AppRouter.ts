0ac8c62a898f91d3a91bff38b1ebc405fe9b9aa6
Edward Alexander Rodríguez Londoño <122028804+ed1808@users.noreply.github.com>
2025-05-25 20:12:47 -0500

    Update routes

--- backend/src/AppRouter.ts
+++ backend/src/AppRouter.ts
@@ -1,12 +1,25 @@
-import { Database } from './core/database/Database';
 import { HttpStatusCode } from './core/router/utils/HttpStatusCodes';
 import { Router } from './core/router/Router';
 
-import type { City } from './locations/models/City';
-import type { Department } from './locations/models/Department';
+import { CitiesService } from './locations/services/CitiesServices';
+import { DepartmentsService } from './locations/services/DepartmentsService';
+import type { DepartmentUpdate } from './locations/models/Department';
+import type { CityUpdate } from './locations/models/City';
+import { CategoriesService } from './categories/services/CategoriesService';
+import type {
+  CategoryCreate,
+  CategoryUpdate,
+} from './categories/models/Category';
+import {
+  validateBody,
+  validateParams,
+} from './core/middlewares/validation_types/ValidationMiddlewares';
+import { categorySchema } from './categories/schemas/CategorySchema';
+import { numericIdParamSchema } from './core/utils/schemas/ParamsSchema';
+import { cityUpdateSchema } from './locations/schemas/CitySchema';
+import { departmentUpdateSchema } from './locations/schemas/DepartmentSchema';
 
 const appRouter = new Router();
-const dbInstance = Database.instance;
 
 /* ||=========================|| */
 /* ||====== Middlewares ======|| */
@@ -25,205 +38,337 @@ appRouter.use(req => {
 /* ||====== Location routes ======|| */
 /* ||=============================|| */
 
+/* ====== Departments routes ====== */
 appRouter.get('/api/locations/departments', async ({ query }) => {
   const { limit, offset } = query;
-  const result = await dbInstance.select<Department[]>(
-    'departments',
-    ['id', 'department_name', 'department_code'],
-    { active: true },
-    { department_name: 'ASC' },
-    offset ? Number(offset) : undefined,
-    limit ? Number(limit) : undefined
-  );
+  const service = new DepartmentsService();
 
-  if (result instanceof Error) {
-    return Response.json(
-      { message: result.message },
-      {
-        status: HttpStatusCode.INTERNAL_SERVER_ERROR,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
-  }
-
-  if (typeof result === 'string') {
-    return Response.json(
-      { message: result },
-      {
-        status: HttpStatusCode.NOT_FOUND,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
-  }
-
-  const departments = result.slice(0, result.length);
+  const result = await service.getDepartments(limit, offset);
 
   return Response.json(
-    { message: departments },
     {
+      message: result.data,
+      error: result.error,
+    },
+    {
+      status: result.status,
       headers: { 'Content-Type': 'application/json' },
     }
   );
 });
 
-appRouter.get('/api/locations/departments/:id', async ({ params }) => {
-  const { id } = params;
-  const result = await dbInstance.select<Department[]>(
-    'departments',
-    ['id', 'department_name', 'department_code'],
-    { id: id! }
-  );
+appRouter.get(
+  '/api/locations/departments/:id',
+  validateParams(numericIdParamSchema),
+  async ({ params }) => {
+    const { id } = params;
+    const departmentId = Number(id);
 
-  if (result instanceof Error) {
+    const service = new DepartmentsService();
+    const result = await service.getDepartmentById(departmentId);
+
     return Response.json(
-      { message: result.message },
       {
-        status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+        message: result.data,
+        error: result.error,
+      },
+      {
+        status: result.status,
         headers: { 'Content-Type': 'application/json' },
       }
     );
   }
+);
 
-  if (typeof result === 'string') {
+appRouter.get(
+  '/api/locations/departments/:id/cities',
+  validateParams(numericIdParamSchema),
+  async ({ params, query }) => {
+    const { limit, offset } = query;
+    const { id } = params;
+    const departmentId = Number(id);
+
+    const service = new CitiesService();
+    const result = await service.getCitiesByDepartment(
+      departmentId,
+      offset,
+      limit
+    );
+
     return Response.json(
-      { message: result },
       {
-        status: HttpStatusCode.NOT_FOUND,
+        message: result.data,
+        error: result.error,
+      },
+      {
+        status: result.status,
         headers: { 'Content-Type': 'application/json' },
       }
     );
   }
+);
 
-  const department = result.slice(0, result.length);
+appRouter.put(
+  '/api/locations/departments/:id',
+  validateParams(numericIdParamSchema),
+  validateBody(departmentUpdateSchema),
+  async ({ req, params }) => {
+    const { id } = params;
+    const departmentId = Number(id);
+    const updateData = (await req.json()) as DepartmentUpdate;
 
-  return Response.json(
-    { message: department },
-    {
-      headers: { 'Content-Type': 'application/json' },
+    if (isNaN(departmentId)) {
+      return Response.json(
+        { message: 'Invalid department id' },
+        {
+          status: HttpStatusCode.BAD_REQUEST,
+          headers: { 'Content-Type': 'application/problem+json' },
+        }
+      );
     }
-  );
-});
 
-appRouter.get('/api/locations/departments/:id/cities', async ({ params, query }) => {
-  const { limit, offset } = query;
-  const { id } = params;
-  
-  const result = await dbInstance.select<City[]>(
-    'cities',
-    ['id', 'city_name', 'city_code'],
-    { department_id: id! },
-    { city_name: 'ASC' },
-    offset ? Number(offset) : undefined,
-    limit ? Number(limit) : undefined
-  )
+    const service = new DepartmentsService();
+    const result = await service.updateDepartment(departmentId, updateData);
 
-  if (result instanceof Error) {
     return Response.json(
-      { message: result.message },
       {
-        status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+        message: result.data,
+        error: result.error,
+      },
+      {
+        status: result.status,
         headers: { 'Content-Type': 'application/json' },
       }
     );
   }
+);
 
-  if (typeof result === 'string') {
+appRouter.delete(
+  '/api/locations/departments/:id',
+  validateParams(numericIdParamSchema),
+  async ({ params }) => {
+    const { id } = params;
+    const departmentId = Number(id);
+
+    const service = new DepartmentsService();
+    const result = await service.deleteDepartment(departmentId);
+
     return Response.json(
-      { message: result },
       {
-        status: HttpStatusCode.NOT_FOUND,
+        message: result.data,
+        error: result.error,
+      },
+      {
+        status: result.status,
         headers: { 'Content-Type': 'application/json' },
       }
     );
   }
+);
 
-  const cities = result.slice(0, result.length);
-
-  return Response.json(
-    { message: cities },
-    {
-      headers: { 'Content-Type': 'application/json' },
-    }
-  );
-});
-
+/* ====== Cities routes ====== */
 appRouter.get('/api/locations/cities', async ({ query }) => {
   const { limit, offset } = query;
-  const result = await dbInstance.select<City[]>(
-    'cities',
-    ['id', 'city_name', 'city_code'],
-    undefined,
-    undefined,
-    offset ? Number(offset) : undefined,
-    limit ? Number(limit) : undefined
-  );
+  const service = new CitiesService();
 
-  if (result instanceof Error) {
-    return Response.json(
-      { message: result.message },
-      {
-        status: HttpStatusCode.INTERNAL_SERVER_ERROR,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
-  }
-
-  if (typeof result === 'string') {
-    return Response.json(
-      { message: result },
-      {
-        status: HttpStatusCode.NOT_FOUND,
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
-  }
-
-  const cities = result.slice(0, result.length);
+  const result = await service.getCities(offset, limit);
 
   return Response.json(
-    { message: cities },
     {
+      message: result.data,
+      error: result.error,
+    },
+    {
+      status: result.status,
       headers: { 'Content-Type': 'application/json' },
     }
   );
 });
 
-appRouter.get('/api/locations/cities/:id', async ({ params }) => {
-  const { id } = params;
-  const result = await dbInstance.select<City[]>(
-    'cities',
-    ['id', 'city_name', 'city_code'],
-    { id: id! }
-  );
+appRouter.get(
+  '/api/locations/cities/:id',
+  validateParams(numericIdParamSchema),
+  async ({ params }) => {
+    const { id } = params;
+    const cityId = Number(id);
 
-  if (result instanceof Error) {
+    const service = new CitiesService();
+    const result = await service.getCityById(cityId);
+
     return Response.json(
-      { message: result.message },
       {
-        status: HttpStatusCode.INTERNAL_SERVER_ERROR,
+        message: result.data,
+        error: result.error,
+      },
+      {
+        status: result.status,
         headers: { 'Content-Type': 'application/json' },
       }
     );
   }
+);
 
-  if (typeof result === 'string') {
+appRouter.put(
+  '/api/locations/cities/:id',
+  validateParams(numericIdParamSchema),
+  validateBody(cityUpdateSchema),
+  async ({ body, params }) => {
+    const { id } = params;
+    const cityId = Number(id);
+    const updateData = body as CityUpdate;
+
+    const service = new CitiesService();
+    const result = await service.updateCity(cityId, updateData);
+
     return Response.json(
-      { message: result },
       {
-        status: HttpStatusCode.NOT_FOUND,
+        message: result.data,
+        error: result.error,
+      },
+      {
+        status: result.status,
         headers: { 'Content-Type': 'application/json' },
       }
     );
   }
+);
 
-  const city = result.slice(0, result.length);
+appRouter.delete(
+  '/api/locations/cities/:id',
+  validateParams(numericIdParamSchema),
+  async ({ params }) => {
+    const { id } = params;
+    const cityId = Number(id);
+
+    const service = new CitiesService();
+    const result = await service.deleteCity(cityId);
+
+    return Response.json(
+      {
+        message: result.data,
+        error: result.error,
+      },
+      {
+        status: result.status,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+);
+
+/* ||===============================|| */
+/* ||====== Categories routes ======|| */
+/* ||===============================|| */
+
+appRouter.get('/api/categories', async ({ query }) => {
+  const { limit, offset } = query;
+  const service = new CategoriesService();
+
+  const result = await service.getCategories(offset, limit);
 
   return Response.json(
-    { message: city },
     {
+      message: result.data,
+      error: result.error,
+    },
+    {
+      status: result.status,
       headers: { 'Content-Type': 'application/json' },
     }
   );
 });
+
+appRouter.get(
+  '/api/categories/:id',
+  validateParams(numericIdParamSchema),
+  async ({ params }) => {
+    const { id } = params;
+    const categoryId = Number(id);
+
+    const service = new CategoriesService();
+    const result = await service.getCategory(categoryId);
+
+    return Response.json(
+      {
+        message: result.data,
+        error: result.error,
+      },
+      {
+        status: result.status,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+);
+
+appRouter.post(
+  '/api/categories',
+  validateBody(categorySchema),
+  async ({ body }) => {
+    const categoryData = body as CategoryCreate;
+    const service = new CategoriesService();
+
+    const result = await service.createCategory(categoryData);
+
+    return Response.json(
+      {
+        message: result.data,
+        error: result.error,
+      },
+      {
+        status: result.status,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+);
+
+appRouter.put(
+  '/api/categories/:id',
+  validateParams(numericIdParamSchema),
+  validateBody(categorySchema),
+  async ({ body, params }) => {
+    const { id } = params;
+    const categoryId = Number(id);
+    const updateData = body as CategoryUpdate;
+
+    const service = new CategoriesService();
+    const result = await service.updateCategory(categoryId, updateData);
+
+    return Response.json(
+      {
+        message: result.data,
+        error: result.error,
+      },
+      {
+        status: result.status,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+);
+
+appRouter.delete(
+  '/api/categories/:id',
+  validateParams(numericIdParamSchema),
+  async ({ params }) => {
+    const { id } = params;
+    const categoryId = Number(id);
+
+    const service = new CategoriesService();
+    const result = await service.deleteCategory(categoryId);
+
+    return Response.json(
+      {
+        message: result.data,
+        error: result.error,
+      },
+      {
+        status: result.status,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+);
 
 export default appRouter;
